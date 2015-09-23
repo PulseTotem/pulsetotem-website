9f5a06af27e9172fe1710612fd9bff3374670c94
Christian Brel <ch.brel@gmail.com>
2015-09-23 18:54:39 +0200

    refs WEBSITE-3 : Added first view for solution presentation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,7 +209,6 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/**/*.js',
           '<%= yeoman.dist %>/styles/**/*.css',
-          '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
--- app/common/controllers/home.js
+++ app/common/controllers/home.js
@@ -13,31 +13,67 @@ angular.module('PulseTotemCommon')
     $scope.ppArray.push({
       image: 'images/home/multi-content.jpg',
       whiteTheme: false,
-      title: 'Créer de l\'information pour diffuser sur des écrans est une perte de temps',
-      subtitle: 'Centralisez vos contenus et diffusez les sur un écran !'
+      title: 'Gagnez du temps sur votre communication',
+      subtitle: 'Avec PulseTotem, centralisez vos contenus et diffusez les sur un écran'
     });
     $scope.ppArray.push({
       image: 'images/home/communaute.jpg',
       whiteTheme: false,
-      title: 'Trop de contenus à maintenir et à renouveller',
-      subtitle: 'Passez au temps réel, utilisez votre communauté et laissez la s\'exprimer sur vos écrans !'
+      title: 'Renforcez le dynamisme de vos écrans',
+      //subtitle: 'Utilisez votre communauté et laissez la contribuer au contenu de vos écrans !'
+      subtitle: 'Laissez votre communauté s\'exprimer en temps réel sur vos écrans'
     });
     $scope.ppArray.push({
       image: 'images/home/interaction.png',
       whiteTheme: false,
-      title: 'Un manque d\'engagement des utilisateurs face aux informations diffusées',
-      subtitle: 'Engagez votre audience avec de l\'interaction !'
+      title: 'Engagez votre audience',
+      subtitle: 'Impliquez vos utilisateurs avec de l\'interaction'
     });
     $scope.ppArray.push({
-      image: 'images/home/equipe.jpg',
+      //image: 'images/home/equipe.jpg',
+      image: 'images/home/dynamiser.jpg',
       whiteTheme: false,
-      title: 'Une bonne communication sur écran est couteuse en temps et complexe à déployer',
-      subtitle: 'Appuyez vous sur l\'équipe de PulseTotem et construisons ensemble la solution qui vous convient !'
+      title: 'Différenciez vous, soyez innovants dans vos communications numériques',
+      subtitle: 'Appuyez vous sur l\'équipe de PulseTotem et construisons ensemble la solution qui vous convient'
     });
-    $scope.ppArray.push({
+
+    /*$scope.ppArray.push({
       image: 'images/home/dynamiser.jpg',
       whiteTheme: false,
       title: 'Dynamisez vos évènements avec l\'ajout d\'écrans interactifs n\'est pas dans votre top priorité',
       subtitle: 'Contactez nous, appuyez vous sur notre expérience et nos compétences en interactions !'
+    });*/
+
+    $scope.avRow1 = [];
+    $scope.avRow1.push({
+      icon: './bower_components/material-design-icons/action/svg/production/ic_history_48px.svg',
+      text: "Temps réel"
     });
+    $scope.avRow1.push({
+      icon: './bower_components/material-design-icons/file/svg/production/ic_cloud_circle_48px.svg',
+      text: "Centralisation des contenus"
+    });
+    $scope.avRow1.push({
+      icon: './bower_components/material-design-icons/av/svg/production/ic_games_48px.svg',
+      text: "Interactions directes"
+    });
+    $scope.avRow1.push({
+      icon: './bower_components/material-design-icons/content/svg/production/ic_archive_48px.svg',
+      text: "Récupération automatisée des contenus existants"
+    });
+
+    $scope.avRow2 = [];
+    $scope.avRow2.push({
+      icon: './bower_components/material-design-icons/notification/svg/production/ic_event_available_48px.svg',
+      text: "Dynamisation des évènements"
+    });
+    $scope.avRow2.push({
+      icon: './bower_components/material-design-icons/action/svg/production/ic_build_48px.svg',
+      text: "Personnalisation"
+    });
+    $scope.avRow2.push({
+      icon: './bower_components/material-design-icons/av/svg/production/ic_airplay_48px.svg',
+      text: "Full Web"
+    });
+
   }]);
