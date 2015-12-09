2baa45afe08115ed2d048802da3b0aa8708ca303
Simon Urli <simon.urli@gmail.com>
2015-12-09 13:52:24 +0100

    WEBSITE-15 Rename images for the SEO and change picture for Simon.

--- app/common/controllers/home.js
+++ app/common/controllers/home.js
@@ -29,7 +29,7 @@ angular.module('PulseTotemCommon')
     });
 
     $scope.ppArray.push({
-      image: 'images/home/communaute_hiweb.jpg',
+      image: 'images/home/communaute.jpg',
       title: 'HOME.MULTICONTENT.TITLE',
       subtitle: 'HOME.MULTICONTENT.SUBTITLE',
       blog: null,
@@ -37,7 +37,7 @@ angular.module('PulseTotemCommon')
     });
 
     $scope.ppArray.push({
-      image: 'images/home/innovation_hiweb.jpg',
+      image: 'images/home/innovation.jpg',
       title: 'HOME.DYNAMIZE.TITLE',
       subtitle: 'HOME.DYNAMIZE.SUBTITLE',
       blog: null,
@@ -48,7 +48,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/av/svg/production/ic_games_48px.svg',
-      icon: './images/solution/svg/web40.svg',
+      icon: './images/solution/svg/interaction.svg',
       title: 'SOLUTION.FEATURES.INTERACTION.TITLE',
       description: 'SOLUTION.FEATURES.INTERACTION.DESCRIPTION',
       showDescription: false
@@ -56,7 +56,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/action/svg/production/ic_history_48px.svg',
-      icon: './images/solution/svg/pc11.svg',
+      icon: './images/solution/svg/temps_reel.svg',
       title: 'SOLUTION.FEATURES.REALTIME.TITLE',
       description: 'SOLUTION.FEATURES.REALTIME.DESCRIPTION',
       showDescription: false
@@ -64,7 +64,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/content/svg/production/ic_archive_48px.svg',
-      icon: './images/solution/svg/manager.svg',
+      icon: './images/solution/svg/reseau_sociaux.svg',
       title: 'SOLUTION.FEATURES.SOCIAL.TITLE',
       description: 'SOLUTION.FEATURES.SOCIAL.DESCRIPTION',
       showDescription: false
@@ -72,7 +72,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/file/svg/production/ic_cloud_circle_48px.svg',
-      icon: './images/solution/svg/marketing8.svg',
+      icon: './images/solution/svg/centralisation.svg',
       title: 'SOLUTION.FEATURES.CENTRALIZATION.TITLE',
       description: 'SOLUTION.FEATURES.CENTRALIZATION.DESCRIPTION',
       showDescription: false
@@ -87,7 +87,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/action/svg/production/ic_build_48px.svg',
-      icon: './images/solution/svg/wrench1.svg',
+      icon: './images/solution/svg/personnalisation.svg',
       title: 'SOLUTION.FEATURES.CUSTOMIZATION.TITLE',
       description: 'SOLUTION.FEATURES.CUSTOMIZATION.DESCRIPTION',
       showDescription: false
@@ -95,7 +95,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/av/svg/production/ic_airplay_48px.svg',
-      icon: './images/solution/svg/onlinestore2.svg',
+      icon: './images/solution/svg/fullweb.svg',
       title: 'SOLUTION.FEATURES.FULLWEB.TITLE',
       description: 'SOLUTION.FEATURES.FULLWEB.DESCRIPTION',
       showDescription: false
@@ -103,7 +103,7 @@ angular.module('PulseTotemCommon')
 
     $scope.avRow1.push({
       //icon: './bower_components/material-design-icons/av/svg/production/ic_airplay_48px.svg',
-      icon: './images/solution/svg/group41.svg',
+      icon: './images/solution/svg/equipe.svg',
       title: 'SOLUTION.FEATURES.TEAM.TITLE',
       description: 'SOLUTION.FEATURES.TEAM.DESCRIPTION',
       showDescription: false
@@ -111,12 +111,12 @@ angular.module('PulseTotemCommon')
 
       $scope.productionsArray = [];
       $scope.productionsArray.push({
-        image: "images/productions/fetedelascience.jpg",
+        image: "images/productions/fetedelascience2015_livre_or_interactif.jpg",
         title: 'PRODUCTIONS.FETESCIENCE.TITLE',
         subtitle: 'PRODUCTIONS.FETESCIENCE.SUBTITLE'
       });
       $scope.productionsArray.push({
-        image: "images/productions/mangameshow.jpg",
+        image: "images/productions/mangameshow2015_selfie.jpg",
         title: 'PRODUCTIONS.MANGAME.TITLE',
         subtitle: 'PRODUCTIONS.MANGAME.SUBTITLE'
       });
